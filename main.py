b1004ce7742be55e16ed1aa4a873993c308b9326
Yan Wang <grapeot@gmail.com>
2014-06-30 12:18:43 -0700

    add a .gitignore

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import urllib2
-import httplib 
-import requests
+import re, sys, requests
 
 
-token = 'ea2985aa5625'
+# board name, return (isHit, isFinished)
+def shot(bn, pos):
+    content = requests.post('https://student.people.co/api/challenge/battleship/{0}/boards/{1}/{2}'.format(token, bn, pos), allow_redirects=True).content
+    isFinished = re.search('"is_finished": true', content) is not None
+    isHit = re.search('"is_hit": true', content) is not None
+    return isHit, isFinished
 
-# board name
-def Shot(bn, pos):
-    req = requests.put('https://student.people.co/api/challenge/battleship/{0}/boards/{1}/{2}'.format(token, bn, pos), allow_redirects=True).content
-
-def Reset(bn):
+def reset(bn):
     return requests.delete('https://student.people.co/api/challenge/battleship/{0}/boards/{1}/'.format(token, bn), allow_redirects=True).content
 
-def BoardInfo(bn):
+def boardInfo(bn):
     return requests.get('https://student.people.co/api/challenge/battleship/{0}/boards/{1}/'.format(token, bn), allow_redirects=True).content
 
-bn = 'test_board_1'
-print Shot(bn, 'A2')
-print BoardInfo(bn)
-print Reset(bn)
-print BoardInfo(bn)
+def isFinished(bn):
+    content = requests.get('https://student.people.co/api/challenge/battleship/{0}/boards/{1}/'.format(token, bn), allow_redirects=True).content
+    return re.search('"is_finished": true', content) is not None
+
+token = 'ea2985aa5625'
+letters = [chr(x) for x in range(ord('A'), ord('Z') + 1)]
+numbers = range(1, 10 + 1)
+
+bns = ['test_board_1' , 'test_board_2' , 'test_board_3' , 'test_board_4' , 'test_board_5']
+for bn in bns:
+    print reset(bn)
+    for l in letters:
+        for n in numbers:
+            result = shot(bn, '{0}{1}'.format(l, n)) 
+            print l, n, result
+            if result[1]:
+                sys.exit(0)
